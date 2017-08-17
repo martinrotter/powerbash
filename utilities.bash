a46be93fa057b2059cb3de75f0793d655343337c
Martin Rotter <rotter@praktik.cz>
2017-08-17 14:37:53 +0200

    Ingore case for completions.

--- utilities.bash
+++ utilities.bash
@@ -2,3 +2,5 @@
 ### This is the OPTIONAL Bash powerbash utlity script.
 #####################################################
 
+
+bind 'set completion-ignore-case on'        # Make completion case insensitive.
